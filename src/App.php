3eea3f8df8e109a1fd20b56a827a2e2f78946aaa
Pierre Bretéché <pbreteche@dawan.fr>
2021-10-13 11:41:43 +0200

    démo php-cs-fixer

--- src/App.php
+++ src/App.php
@@ -5,16 +5,14 @@ namespace App;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-class App
-{
-    public function exec()
-    {
+class App {
+    public function exec() {
         // create a log channel
-        $log = new Logger('app');
+        $log = new Logger( 'app' );
         $log->pushHandler(new StreamHandler(__DIR__.'/../var/dev.log', Logger::WARNING));
 
         // add records to the log
-        $log->warning('Foo');
+        $log -> warning('Foo');
         $log->error('Bar');
     }
 }
